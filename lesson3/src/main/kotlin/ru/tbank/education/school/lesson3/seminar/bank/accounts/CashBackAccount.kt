4e1f57622b57a530f8433d54bf2e9ac801951deb
DimCher10 <103523226+DimCher10@users.noreply.github.com>
2025-09-20 13:40:31 +0300

    Create CashBackAccount.kt

---
+++ lesson3/src/main/kotlin/ru/tbank/education/school/lesson3/seminar/bank/accounts/CashBackAccount.kt
@@ -0,0 +1,6 @@
+package ru.tbank.education.school.lesson3.seminar.bank.accounts
+
+import ru.tbank.education.school.lesson3.seminar.bank.models.Currency
+import ru.tbank.education.school.lesson3.seminar.bank.models.Customer
+import ru.tbank.education.school.lesson3.seminar.bank.models.DepositTransaction
+import ru.tbank.education.school.lesson3.seminar.bank.models.WithdrawalTransaction
